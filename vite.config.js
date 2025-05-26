339fb1a0a3e87c2d89d7db36293ab307cfe23be3
Matthew Thai <thaimatthew18@gmail.com>
2025-05-26 19:15:11 +1000

    Fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/About-me-website",
+  base: "/About-me-website/",
   plugins: [
     react(),
   ],
