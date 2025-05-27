c173d2e8727f84e522de46a1ccffea280337ca02
Matthew Thai <thaimatthew18@gmail.com>
2025-05-27 17:43:14 +1000

    Fixed 404 error with homepage

--- src/UI_elements/navbar.jsx
+++ src/UI_elements/navbar.jsx
@@ -4,7 +4,7 @@ import homeIcon from "../assets/icons8-home.svg"
 function NavBar() {
   return (
     <nav class="p-4 flex justify-between">
-      <a href="/About-me-website">
+      <a href="/About-me-website/">
         <img src={homeIcon} alt="Home" height="25" width="25"/>
       </a>
       <ul class="flex flex-row">
